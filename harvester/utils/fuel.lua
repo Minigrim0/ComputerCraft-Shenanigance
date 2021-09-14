cc2127a36550d3d83feac2d5159da22f58d13402
Minigrim0 <grimauflorent@gmail.com>
2021-09-14 16:59:32 +0200

    [Add] fuel utils

---
+++ harvester/utils/fuel.lua
@@ -0,0 +1,15 @@
+-- Refuels the turtle if needed, taking logs in case the fuel slot is empty
+local function refuel(limit)
+    local fuelAmount = turtle.getFuelLevel()
+    if fuelAmount < limit then
+        turtle.select(FUEL_SLOT)
+        if turtle.getItemCount == 0 then
+            -- Get some wood and move it to the fuel case
+            turtle.select(1)
+            turtle.transferTo(FUEL_SLOT, 2)
+            turtle.select(FUEL_SLOT)
+        end
+        turtle.refuel()
+    end
+    turtle.select(1)
+end
