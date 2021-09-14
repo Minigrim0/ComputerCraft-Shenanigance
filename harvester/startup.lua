97232ac3ffa040796d9581d1631e40ad126b13ad
Minigrim0 <grimauflorent@gmail.com>
2021-09-14 17:17:25 +0200

    [Fix] missing another counter + moved github url in variable

--- harvester/startup.lua
+++ harvester/startup.lua
@@ -1,11 +1,12 @@
 local installed_libs = {"utils/fuel.lua", "utils/movement.lua", "harvester.lua"}
 
 START_POS = {0, 0, 0}  -- SET This with the turtle's initial position
+GITHUB_BASE_URL = "https://raw.githubusercontent.com/Minigrim0/ComputerCraft-Shenanigance/main/harvester/"
 
 local function preinit()
     for counter=1,#installed_libs do
         if io.open(installed_libs[counter], "r") == nil then
-            shell.run("wget", "https://raw.githubusercontent.com/Minigrim0/ComputerCraft-Shenanigance/main/harvester/" .. installed_libs[counter], installed_libs)
+            shell.run("wget", GITHUB_BASE_URL .. installed_libs[counter], installed_libs[counter])
         end
     end
 end
