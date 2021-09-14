33d3f440625e9b8322f5264572366cd6b5db7888
Minigrim0 <grimauflorent@gmail.com>
2021-09-14 17:10:06 +0200

    [Upd] startup to maintain the code

--- harvester/startup.lua
+++ harvester/startup.lua
@@ -1,41 +1,19 @@
-local movement = require "utils/movement"
+local installed_libs = {"utils/fuel.lua", "utils/movement.lua", "harvester.lua"}
 
 START_POS = {0, 0, 0}  -- SET This with the turtle's initial position
 
 local function preinit()
-    if io.open("utils/fuel.lua", "r") == nil then
-        shell.run("wget", "")
-    end
-end
-
--- Checks that the turtle is in the right place
-local function init()
-    movement.MoveTo(START_POS)
-    local success, bloc_down = turtle.inspectDown()
-    if success and bloc_down.name == "minecraft:gravel" then
-        local _success, bloc_front = turtle.inspect()
-        local counter = 0
-
-        while success or bloc_front.name ~= "minecraft:chest" do
-            turtle.turnLeft()
-            counter = counter + 1
-            if counter == 4 then
-                return false
-            end
+    for counter=1,#installed_libs do
+        if io.open(installed_libs[counter], "r") == nil then
+            shell.run("wget", "https://github.com/Minigrim0/ComputerCraft-Shenanigance/blob/main/harvester/" .. installed_libs, installed_libs)
         end
-        io.write("Sucessful initialization")
-        return true
-    end
-    return false
-end
-
-local function main()
-    if ~init() then
-        return false
     end
 end
 
-local status = main()
+preinit()
+local harvester = require 'harvester'
+
+local status = harvester.main()
 if ~status then
     io.write("Program ended with an error")
 end
