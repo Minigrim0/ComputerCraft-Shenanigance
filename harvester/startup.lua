c7a30f59a63e708061a065854fe6a193230d2347
Minigrim0 <grimauflorent@gmail.com>
2021-09-14 17:15:35 +0200

    [Fix] do not concatenate array with string

--- harvester/startup.lua
+++ harvester/startup.lua
@@ -5,7 +5,7 @@ START_POS = {0, 0, 0}  -- SET This with the turtle's initial position
 local function preinit()
     for counter=1,#installed_libs do
         if io.open(installed_libs[counter], "r") == nil then
-            shell.run("wget", "https://raw.githubusercontent.com/Minigrim0/ComputerCraft-Shenanigance/main/harvester/" .. installed_libs, installed_libs)
+            shell.run("wget", "https://raw.githubusercontent.com/Minigrim0/ComputerCraft-Shenanigance/main/harvester/" .. installed_libs[counter], installed_libs)
         end
     end
 end
