d22a801d4755a62484970052fe79d9d4ea079d39
Minigrim0 <grimauflorent@gmail.com>
2021-09-14 17:32:15 +0200

    [Upd] movement utils + made exportable

--- harvester/utils/movement.lua
+++ harvester/utils/movement.lua
@@ -1,24 +1,55 @@
+local fuel = require 'utils/fuel'
+
 -- Wrapper to move the turtle and keep it fueled
-function MoveTo(direction, amount)
-    for step=1,amount do
-        refuel(50)
-        if direction == "up" then
-            turtle.up()
-        elseif direction == "down" then
-            turtle.down()
-        elseif direction == "forward" then
+local move = function(direction, amount)
+    if direction == "backward" then
+        turtle.turnLeft()
+        turtle.turnLeft()
+        for _step=1,amount do
+            fuel.Refuel(50)
             turtle.forward()
-        else
-            print("No such direction ", direction)
+        end
+        turtle.turnLeft()
+        turtle.turnLeft()
+    else
+        for step=1,amount do
+            fuel.refuel(50)
+            if direction == "up" then
+                turtle.up()
+            elseif direction == "down" then
+                turtle.down()
+            elseif direction == "forward" then
+                turtle.forward()
+            else
+                print("No such direction ", direction)
+            end
         end
     end
 end
 
-function Orientation()
+local orientation = function()
     local pos_x, _pos_y, pos_z = gps.locate()
-    Move("forward", 1)
+    move("forward", 1)
+    local npos_x, _npos_y, npos_z = gps.locate()
+    move("backward", 1)
+
+    if pos_x < npos_x then
+        return "E"
+    elseif npos_x < pos_x then
+        return "O"
+    elseif pos_z < npos_z then
+        return "S"
+    elseif npos_z < pos_z then
+        return "N"
+    end
 end
 
-function MoveTo(position)
-    
+local moveTo = function(position)
+    local turtle_orientation = orientation()
 end
+
+return {
+    MoveTo=moveTo,
+    Orientation=orientation,
+    move=move
+}
