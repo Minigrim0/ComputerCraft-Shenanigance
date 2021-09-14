38661d20cadc8a0ddb091578e7f24c6dad9ba911
Minigrim0 <grimauflorent@gmail.com>
2021-09-14 17:10:16 +0200

    [Add] harvester function

---
+++ harvester/harvester.lua
@@ -0,0 +1,29 @@
+
+local movement = require "utils/movement"
+
+-- Checks that the turtle is in the right place
+local function init()
+    movement.MoveTo(START_POS)
+    local success, bloc_down = turtle.inspectDown()
+    if success and bloc_down.name == "minecraft:gravel" then
+        local _success, bloc_front = turtle.inspect()
+        local counter = 0
+
+        while success or bloc_front.name ~= "minecraft:chest" do
+            turtle.turnLeft()
+            counter = counter + 1
+            if counter == 4 then
+                return false
+            end
+        end
+        io.write("Sucessful initialization")
+        return true
+    end
+    return false
+end
+
+function Main()
+    if ~init() then
+        return false
+    end
+end
